fe7891b8c93da02e23738eecf3353bee6584c964
Stephen Howard <Showard1793@gmail.com>
2024-10-28 20:11:02 -0500

    Updated

--- src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -1,7 +1,7 @@
 package org.launchcode.codingevents.data;
 
-import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.data.repository.CrudRepository;
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.stereotype.Repository;
 
 /**
